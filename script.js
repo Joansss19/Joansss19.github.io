8df230d7e29f3fa8b629690239cffb3c0942bf91
Joansss19 <sitojoan@icloud.com>
2025-09-05 14:54:05 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function changeText() {
-  const text = document.getElementById("dynamicText");
+function changeHeroText() {
+  const text = document.getElementById("heroText");
   const frases = [
     "🌟 La creatividad no tiene límites.",
-    "🚀 Bootstrap + JS = páginas interactivas.",
-    "🔥 Tú puedes personalizar todo esto.",
-    "✨ Aprende creando proyectos."
+    "🚀 Bootstrap + JS = experiencias web interactivas.",
+    "🔥 Personaliza todo a tu gusto.",
+    "✨ Aprende y diviértete creando."
   ];
   text.innerText = frases[Math.floor(Math.random() * frases.length)];
 }
